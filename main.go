9a8e58653e5c7abb7119550185953b4ff833a7cf
whitehai11 <me.goth@icloud.com>
2026-03-16 00:20:56 +0100

    Fix installer Wails Linux build

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"log"
-
 	"github.com/whitehai11/AWaN-Installer/installer"
 	"github.com/whitehai11/AWaN-Installer/ui/gui"
-	"github.com/whitehai11/AWaN-Installer/ui/tui"
 	"github.com/whitehai11/AWaN-Installer/utils"
 )
 
@@ -13,27 +10,10 @@ func main() {
 	logger := utils.NewLogger()
 	flowInstaller, err := installer.New(logger)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	environment := installer.DetectEnvironment()
-	logger.Log("INSTALL", "Detected environment "+environment)
-
-	switch environment {
-	case installer.EnvironmentDesktop:
-		if err := gui.Run(flowInstaller); err != nil {
-			logger.Log("INSTALL", "Falling back to terminal installer: "+err.Error())
-			if err := tui.RunInstaller(flowInstaller); err != nil {
-				log.Fatal(err)
-			}
-		}
-	case installer.EnvironmentServer, installer.EnvironmentUnknown:
-		if err := tui.RunInstaller(flowInstaller); err != nil {
-			log.Fatal(err)
-		}
-	default:
-		if err := tui.RunInstaller(flowInstaller); err != nil {
-			log.Fatal(err)
-		}
+	if err := gui.Run(flowInstaller); err != nil {
+		panic(err)
 	}
 }
